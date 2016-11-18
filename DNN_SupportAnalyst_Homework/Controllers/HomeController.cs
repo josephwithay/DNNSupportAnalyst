cb0d0670abdc415162ef1fd6e32ac213939b3736
josephwithay <yoseph_jo@hotmail.com>
2016-11-18 00:00:17 -0800

    Update database working but table won't automatically refresh

--- DNN_SupportAnalyst_Homework/Controllers/HomeController.cs
+++ DNN_SupportAnalyst_Homework/Controllers/HomeController.cs
@@ -1,16 +1,17 @@
-﻿using DNN_SupportAnalyst_Homework.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BridgeLayer;
 
 namespace DNN_SupportAnalyst_Homework.Controllers
 {
     public class HomeController : Controller
     {
-        private DNNHomeworkEntities UserDB = new DNNHomeworkEntities();
-
         public ActionResult Index()
         {
             return View();
@@ -19,15 +20,45 @@ namespace DNN_SupportAnalyst_Homework.Controllers
         // GET: Userlist
         public ActionResult UserList()
         {
-            var users = this.UserDB.Users;
+            UsersBridgeLayer usersBridgeLayer = new UsersBridgeLayer();
+            List<User> users = usersBridgeLayer.Users.ToList();
             ViewBag.Message = "List of users currently registered";
-            return this.View(users);
+
+            return View(users);
         }
 
         // GET: CreateUser
+        [HttpGet]
         public ActionResult Register()
         {
             return View();
+        }
+
+        // POST: Users/CreateUser
+        [HttpPost]
+        public ActionResult Register(string firstName, string lastName, string userName, string email)
+        {
+            User user = new User();
+            user.firstName = firstName;
+            user.lastName = lastName;
+            user.userName = userName;
+            user.email = email;
+
+            UsersBridgeLayer userBridgeLayout = new UsersBridgeLayer();
+            userBridgeLayout.AddUser(user);
+
+            return View();
+            /*
+             * Just for debugging purposes
+            if (ModelState.IsValid)
+            {
+                foreach (string key in formCollection.AllKeys)
+                {
+                    Response.Write("Key = " + key + "  ");
+                    Response.Write("Value = " + formCollection[key]);
+                    Response.Write("<br/>");
+                }
+            } */           
         }
     }
 }
